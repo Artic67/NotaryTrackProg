8c7482994dd8a3d9ed5ecf5f3397d222840054b5
Artic67 <sirartic67@gmail.com>
2021-12-26 15:32:45 +0200

    Design changes1

--- notary-track-prog/src/diagram.js
+++ notary-track-prog/src/diagram.js
@@ -1,4 +1,14 @@
 export function drawDiagram() {
+    let gradient;
+    
+    function getGradient(ctx, chartArea, color0, color1) {
+      gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+      gradient.addColorStop(0, color0);
+      gradient.addColorStop(1, color1);
+    
+      return gradient;
+    }
+
     const ctx = document.getElementById('myChart').getContext('2d');
 
     const labels = ['01.12', '02.12', '03.12', '04.12', '05.12', '06.12', '07.12', '08.12', '09.12', '10.12'];
@@ -8,23 +18,43 @@ export function drawDiagram() {
         {
           label: 'Прибыль',
           data: [500, 2000, 700, 1300, 400, 1000, 1500, 1700, 2200, 1400],
-          borderColor: [
-            'rgba(255, 99, 132, 1)'
-        ],
+          borderColor: function(context) {
+            const chart = context.chart;
+            const {ctx, chartArea} = chart;
+
+            if (!chartArea) {
+              // This case happens on initial chart load
+              return;
+            }
+            return getGradient(ctx, chartArea, '#6F04D9', '#05C7F2');
+          },
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)'
-        ],
+            'rgba(54, 162, 235, 0.2)'
+          ],
+          cubicInterpolationMode: 'monotone',
+          tension: 0.4,
           yAxisID: 'y',
         },
         {
           label: 'Количество клиентов',
           data: [1, 2, 4, 2, 2, 5, 6, 7, 3, 5],
-          borderColor: [
-            'rgba(54, 162, 235, 1)',
-        ],
+          borderColor: function(context) {
+            console.log(context);
+            const chart2 = context.chart;
+            console.log(chart2);
+            const {ctx, chartArea} = chart2;
+
+            if (!chartArea) {
+              // This case happens on initial chart load
+              return;
+            }
+            return getGradient(ctx, chartArea, '#FF0000', '#FFAE00');
+          },
           backgroundColor: [
-            'rgba(54, 162, 235, 0.2)',
-        ],
+            'rgba(255, 99, 132, 0.2)'
+          ],
+          cubicInterpolationMode: 'monotone',
+          tension: 0.4,
           yAxisID: 'y1',
         }
       ]
--- notary-track-prog/src/index.js
+++ notary-track-prog/src/index.js
@@ -59,5 +59,9 @@ app.on('activate', () => {
   }
 });
 
+
+app.on('closeProgram', () => {
+  app.quit();
+});
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
