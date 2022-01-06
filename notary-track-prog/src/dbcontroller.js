c636eafb2bf1ab18fa58be2f072d7bc03492f4ee
Artic67 <sirartic67@gmail.com>
2022-01-06 21:50:41 +0200

    Deleted unnecessary files

--- notary-track-prog/src/dbcontroller.js
+++ notary-track-prog/src/dbcontroller.js
@@ -1,5 +1,6 @@
 export const ADODB = require('node-adodb');
-export const connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=./src/Access.accdb;Persist Security Info=False;');
+let filesource = "./src/Access.accdb";
+export const connection = ADODB.open(`Provider=Microsoft.ACE.OLEDB.12.0${filesource};Data Source=;Persist Security Info=False;`);
 
  
 export let DBController = {
@@ -24,31 +25,3 @@ export let DBController = {
         }
     }
 }
-
-// get the client
-/*const mysql = require('mysql2');
-
-let Controller = {
-    createQuery: function createQuery(query, asArray=false) {
-        const connection = mysql.createConnection({
-            host: 'sql11.freesqldatabase.com',
-            user: 'sql11461053',
-            database: 'sql11461053',
-            password: 'RZzDUA4MhS',
-            rowsAsArray: asArray
-          });
-
-        return connection.promise()
-        .query(query).then(connection.end());
-        .then(( [rows, fields]) => {
-            console.log(rows);
-            window.DB = rows;
-        })
-        .catch(console.log)
-        .then(connection.end());
-    }
-}
-
-let promise = Controller.createQuery('SELECT * FROM `q`', false);
-console.log(promise);
-promise.then( ([rows, fields]) => console.log(rows));*/
